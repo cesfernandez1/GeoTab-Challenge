dc688bb6fff592aaf5d610f548d73c9f4245c047
Cesar Fernandez <32900036+cesfernandez1@users.noreply.github.com>
2022-06-04 21:35:05 +0100

    Developed the entire program, the code needs to be refactored

--- GeoTab-Challenge/GeoTab-Challenge/Program.cs
+++ GeoTab-Challenge/GeoTab-Challenge/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Threading;
 using Geotab.Checkmate;
 using Geotab.Checkmate.ObjectModel;
+using Geotab.Checkmate.ObjectModel.Engine;
 
 namespace GeoTab_Challenge
 {
@@ -47,9 +49,9 @@ namespace GeoTab_Challenge
             {
                 Console.WriteLine("Downloading data...");
 
-                List<Device> vehicles = await api.CallAsync<List<Device>>("Get", typeof(Device));
+                List<Device> devices = await api.CallAsync<List<Device>>("Get", typeof(Device));
 
-                foreach (Device device in vehicles)
+                foreach (GoDevice device in devices)
                 {
                     string filename = device.SerialNumber + ".csv";
                     string path = Path.Combine(folder, filename);
@@ -62,12 +64,47 @@ namespace GeoTab_Challenge
                         File.WriteAllText(path, "ID,Timestamp, VIN,Coordinates,Odometer \n");
                     }
 
-                    using (StreamWriter writer = new StreamWriter(path, true))
+                    var results = await api.CallAsync<List<DeviceStatusInfo>>("Get", typeof(DeviceStatusInfo), new
                     {
-                        writer.WriteLine($"{device.SerialNumber}, {DateTime.UtcNow}");
+                        search = new DeviceStatusInfoSearch
+                        {
+                            DeviceSearch = new DeviceSearch
+                            {
+                                Id = device.Id
+                            }
+                        }
+                    });
+
+                    if(results.Count <= 0)
+                    {
+                        continue;
+                    }
+
+                    var statusDataSearch = new StatusDataSearch
+                    {
+                        DeviceSearch = new DeviceSearch(device.Id),
+                        DiagnosticSearch = new DiagnosticSearch(KnownId.DiagnosticOdometerAdjustmentId),
+                        FromDate = DateTime.MaxValue
                     };
 
-                    Console.WriteLine("Serial number : " + device.SerialNumber);
+                    // Retrieve the odometer status data
+                    IList<StatusData> statusData = await api.CallAsync<IList<StatusData>>("Get", typeof(StatusData), new { search = statusDataSearch });
+
+                    var odometerREadings = statusData[0].Data ?? 0;
+
+                    DeviceStatusInfo deviceStatus = results[0];
+
+                    var isMetric = RegionInfo.CurrentRegion.IsMetric;
+
+
+                    using (StreamWriter writer = new StreamWriter(path, true))
+                    {
+                        string coordinates = string.Format("(xy) {0}   {1}", deviceStatus.Latitude, deviceStatus.Longitude);
+
+                        var odometer = Math.Round(isMetric ? odometerREadings : Distance.ToImperial(odometerREadings / 1000), 0);
+
+                        writer.WriteLine($"{device.Id}, {DateTime.Now} , {device.SerialNumber} , {coordinates} , {odometer}");
+                    };
                 }
 
                 Thread.Sleep(1000);
